7f628203b369b8f21e5401e82b4f2e6d365eb78f
Alexandr <61269662+HIkE16@users.noreply.github.com>
2025-03-04 07:54:02 +0000

    Changes to be committed black: 	modified:   databases.py

--- databases.py
+++ databases.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./recipe.db"
 engine = create_async_engine(url=DATABASE_URL, echo=True)
 
-async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
 
 session = async_session()
 
